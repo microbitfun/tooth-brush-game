70e2b9b8806a09bce74bf0fc91cad1e36eacdb2e
MicrobitFun <68689935+microbitfun@users.noreply.github.com>
2020-08-19 13:30:55 +0200

    changed reset

--- main.ts
+++ main.ts
@@ -2,11 +2,9 @@ input.onButtonPressed(Button.A, function () {
     zprevious = input.acceleration(Dimension.Z)
     start = 1
     count = 0
-    game.startCountdown(120000)
+    game.startCountdown(1000)
     starttime = control.millis()
-})
-input.onButtonPressed(Button.B, function () {
-    control.reset()
+    basic.showString("S")
 })
 let z = 0
 let starttime = 0
@@ -16,9 +14,12 @@ let start = 0
 radio.setTransmitPower(7)
 radio.setGroup(1)
 start = 0
+basic.showString("S")
 basic.forever(function () {
     if (game.isGameOver()) {
         game.setScore(count)
+        basic.pause(20000)
+        control.reset()
     } else {
         if (start == 1) {
             zprevious = z
@@ -36,6 +37,7 @@ basic.forever(function () {
             radio.sendValue("count", count)
             radio.sendValue("time", (control.millis() - starttime) / 1000)
             basic.pause(100)
+            led.toggle(2, 2)
         }
     }
 })
